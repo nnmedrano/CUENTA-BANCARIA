e4be67e37b2c1ffa166a14c758a506da4f858793
nicole <nnmedrano94@hotmail.com>
2017-10-27 20:27:18 -0300

    Ingreso de Clases

---
+++ TITULAR.cs
@@ -0,0 +1,12 @@
+using System;
+
+       public class Titular
+       {
+           private string nombreCompleto{get,set};
+           public long DNI{get,set};
+           private string mail{get,set};
+           private int telefono{get,set};
+           public int DateTime{get,set};
+       }
+     
+    
